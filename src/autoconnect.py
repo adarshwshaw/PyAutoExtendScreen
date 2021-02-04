e54da91bc6606de8d9237ba03163cdbad0b57831
Adarsh <adarshwshaw@gmail.com>
2021-02-05 00:31:21 +0530

    update the autogui code to keyboard shortcuts

--- src/autoconnect.py
+++ src/autoconnect.py
@@ -1,12 +1,18 @@
 import pyautogui as pyag
 import time
 
-pyag.hotkey('win','p')
-WIDTH,HEIGHT = pyag.size()
-print(WIDTH," ",HEIGHT);
-pyag.moveTo(int(WIDTH*0.85),int(HEIGHT*0.58),0.2)
-time.sleep(0.2)
-pyag.click()
-pyag.moveTo(int(WIDTH*0.85),int(HEIGHT*0.20))
-time.sleep(3)
-pyag.click()
+# pyag.hotkey('win','p')
+# WIDTH,HEIGHT = pyag.size()
+# print(WIDTH," ",HEIGHT);
+# pyag.moveTo(int(WIDTH*0.85),int(HEIGHT*0.58),0.2)
+# time.sleep(0.6)
+# pyag.click()
+# pyag.moveTo(int(WIDTH*0.85),int(HEIGHT*0.20))
+# time.sleep(3)
+# pyag.click()
+
+pyag.hotkey('win','k')
+time.sleep(0.1)
+pyag.hotkey('shift','\t');
+time.sleep(1)
+pyag.typewrite(['\t','\n'],interval=0.1)    
